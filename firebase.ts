52a257ae0a0d157d33701ac700d246c34c45bca4
snbhante <sarbanandabhikkhu@gmail.com>
2026-09-12 19:14:03 +0600

    Removed unnecesary comments

--- firebase.ts
+++ firebase.ts
@@ -1,35 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getAuth } from "firebase/auth";
-// import { getDatabase } from "firebase/database";
-
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyA86e4C-l9uNLziBJLSvw6zYun6vcln574",
-//     authDomain: "todo-app-2026-09.firebaseapp.com",
-//     databaseURL: "https://todo-app-2026-09.firebaseio.com",
-//     projectId: "todo-app-2026-09",
-//     storageBucket: "todo-app-2026-09.firebasestorage.app",
-//     messagingSenderId: "707143765980",
-//     appId: "1:707143765980:web:3f2364da82f1c0b0fa0919",
-//     measurementId: "G-SKC4GSCMHK"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// export const auth = getAuth(app);
-// export const db = getDatabase(app);
-
-
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
